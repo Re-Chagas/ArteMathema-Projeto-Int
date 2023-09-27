4ad071a4b390d4b1341fb7816152f3d408e8cb36
Renato-Chagas-De-Moraes <renatochagas.m@gmail.com>
2023-09-27 14:23:05 -0300

    terminei i e menu all pages

---
+++ JS/Index.js
@@ -0,0 +1,10 @@
+//Tela de Carregamento
+
+setTimeout(() => {
+    const telaCarregamento = document.getElementById('tela-carregamento');
+    telaCarregamento.style.transition = 'opacity 1s';
+    telaCarregamento.style.opacity = 0;
+    setTimeout(() => {
+      telaCarregamento.style.display = 'none';
+    }, 500);
+  }, 1000);
