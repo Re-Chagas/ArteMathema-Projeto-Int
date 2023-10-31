d4da446529091a33e4e24dfa3de253065716ad9e
Re-Chagas <renatochagas.m@gmail.com>
2023-10-30 22:10:51 -0300

    novo menu, responsividade

--- JS/Index.js
+++ JS/Index.js
@@ -6,6 +6,26 @@ setTimeout(() => {
     telaCarregamento.style.opacity = 0;
     setTimeout(() => {
       telaCarregamento.style.display = 'none';
-    }, 500);
-  }, 1000);
+    }, 0);
+  }, 0);
 
+//menu overlay
+
+var OpenMenu = document.getElementById("open-menu");
+
+OpenMenu.addEventListener("click", function() {
+  document.getElementById("overlay").style.display = "flex";
+});
+
+document.addEventListener("click", function(event) {
+  if (event.target != OpenMenu && event.target.parentNode != OpenMenu) {
+    document.getElementById("overlay").style.display = "none";
+  }
+});
+
+document.querySelectorAll(".dropdown a").forEach(function(el) {
+  el.addEventListener("click", function() {
+    var dropdown = el.parentNode.querySelector(".dropdown-content");
+    dropdown.classList.toggle("active");
+  });
+});
